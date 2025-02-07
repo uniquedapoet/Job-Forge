d383f5cd3c1668dc2da9fd87dd2a8b14982fafa5
JacksonGiemza <jackson.giemza@gmail.com>
2025-02-06 22:54:33 -0700

    previous stuff, shit fucked up on the scaper ill fix later

--- backend/services/job_scraper.py
+++ backend/services/job_scraper.py
@@ -4,19 +4,19 @@
 import csv
 from jobspy import scrape_jobs
 
-jobs = scrape_jobs(
-    # site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor", "google"],
-    site_name=['linkedin']
-    search_term="software engineer",
-    google_search_term="software engineer jobs near San Francisco, CA since yesterday",
-    location="San Francisco, CA",
+def getJobsData(job_title: str, location: str):
+    jobs = scrape_jobs(
+    site_name=['indeed'], # others may require proxies: "linkedin", "zip_recruiter", "glassdoor", "google"
+    search_term=job_title,
+    location=location,
     results_wanted=20,
     hours_old=72,
     country_indeed='USA',
     
     # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
     # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
-)
-print(f"Found {len(jobs)} jobs")
-print(jobs.head())
-# jobs.to_csv("jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
+    )
+    print(f"Found {len(jobs)} jobs")
+
+jobs = getJobsData("Data Scientist", "Denver, CO")
+jobs.to_csv("backend\data\jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
