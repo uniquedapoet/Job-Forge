5f7d06849983431644dfb7c8316f1c049eebcd2e
Jackson Giemza <jackson.giemza@gmail.com>
2025-02-07 18:06:44 -0700

    fixing shit, forgot return statement in job scraper, added generalized file path bcs relative wasnt working and to accomidate mac and pc.... fu;ck

--- backend/services/job_scraper.py
+++ backend/services/job_scraper.py
@@ -2,7 +2,15 @@
 # https://github.com/Bunsly/JobSpy/tree/main
 
 import csv
+import os
 from jobspy import scrape_jobs
+
+# get path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+backend_dir = os.path.dirname(script_dir)
+data_dir = os.path.join(backend_dir, "data")
+file_path = os.path.join(data_dir, "jobs.csv")
+
 
 def getJobsData(job_title: str, location: str):
     jobs = scrape_jobs(
@@ -11,12 +19,17 @@ def getJobsData(job_title: str, location: str):
     location=location,
     results_wanted=20,
     hours_old=72,
-    country_indeed='USA',
+    country_indeed='USA'
     
     # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
     # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
     )
     print(f"Found {len(jobs)} jobs")
+    return jobs
 
 jobs = getJobsData("Data Scientist", "Denver, CO")
-jobs.to_csv("backend\data\jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
+
+jobs.to_csv(file_path, quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_excel
+
+
+
