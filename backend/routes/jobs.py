36040ffeead702fb01e55c4b737235b07edf93ad
JacksonGiemza <jackson.giemza@gmail.com>
2025-02-20 13:38:38 -0700

    fixing file pathing

--- backend/routes/jobs.py
+++ backend/routes/jobs.py
@@ -18,35 +18,37 @@ JOBS_PATH = os.path.join(data_dir, "csvs", "jobs.csv")
 
 
 def get_score(user_id, job_posting_id):
-    try:
+    # try:
         # get job description from jobs csv using job_posting_id
         # TODO: create job_posting_id column in jobs csv
 
-        jobs = pd.read_csv(JOBS_PATH)
-        job_description = jobs.loc[jobs["id"] == job_posting_id, "description"][0]
+    jobs = pd.read_csv(JOBS_PATH)
+    job_description = jobs.loc[jobs["id"] == job_posting_id, "description"][0]
 
-        resume_data = db_tools.get_resumes_by_user_id(user_id)
-        resume_file_name = resume_data[0]['filename']
-        RESUME_PATH = os.path.join("backend", "data", "csvs", resume_file_name)
-        raw_resume = extract_text_from_pdf(RESUME_PATH)
+    resume_data = db_tools.get_resumes_by_user_id(user_id)
+    resume_file_name = resume_data[0]['filename']
+    RESUME_PATH = os.path.join(data_dir, "resumes", resume_file_name)
+    raw_resume = extract_text_from_pdf(RESUME_PATH)
 
-        score_obj = Score(raw_resume, job_description)
-        similarity_score = score_obj.compute_similarity()
+    score_obj = Score(raw_resume, job_description)
+    similarity_score = score_obj.compute_similarity()
+    return similarity_score
 
-        return {
-            "status": "success",
-            "user_id": user_id,
-            "job_posting_id": job_posting_id,
-            "score": similarity_score,
-            "message": "Similarity score computed successfully"
-        }
+    #     return {
+    #         "status": "success",
+    #         "user_id": user_id,
+    #         "job_posting_id": job_posting_id,
+    #         "score": similarity_score,
+    #         "message": "Similarity score computed successfully"
+    #     }
     
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": "An error occurred while computing similarity score",
-            "error": str(e)
-        }
-
+    # except Exception as e:
+    #     return {
+    #         "status": "error",
+    #         "message": "An error occurred while computing similarity score",
+    #         "error": str(e)
+    #     }
+    
 if __name__ == "__main__":
     print(get_score(1,"in-b26a372f08fef696"))
+
