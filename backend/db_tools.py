c97c12b1f33241539dcb21d168390cc6e6625929
JacksonGiemza <jackson.giemza@gmail.com>
2025-02-20 10:35:21 -0700

    stuff, and things

--- backend/db_tools.py
+++ backend/db_tools.py
@@ -1,5 +1,5 @@
 import sqlite3
-from config import USER_DATABASE_URL
+from config import USER_DATABASE_URL, RESUME_DATABASE_URL
 
 def get_user_by_id(user_id):
         """Fetches a single users data by User ID."""
@@ -15,3 +15,18 @@ def get_user_by_id(user_id):
         conn.close()
 
         return dict(zip(column_names, user)) if user else None
+
+def get_resumes_by_user_id(user_id):
+    """Fetches all resumes for a given user ID."""
+    conn = sqlite3.connect(RESUME_DATABASE_URL)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM resumes WHERE user_id = ?", (user_id,))
+    resumes = cursor.fetchall()
+
+    # Get column names
+    column_names = [desc[0] for desc in cursor.description]
+
+    conn.close()
+
+    return [dict(zip(column_names, resume)) for resume in resumes]
--- backend/routes/jobs.py
+++ backend/routes/jobs.py
@@ -1,21 +1,31 @@
 # Scrapes job descriptions from URLs.
 # Stores job applications in the database.
+import sys
+import os
+
+# Append the backend directory to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from resume import extract_text_from_pdf
 from services.score import Score
 import pandas as pd
-import os
+import db_tools
 
-def get_score(user_id, job_posting_id, resume_path):
+
+def get_score(user_id, job_posting_id):
     try:
         # get job description from jobs csv using job_posting_id
         # TODO: create job_posting_id column in jobs csv
-        file_path = os.path.join("backend", "data", "csvs", "jobs.csv")
-        jobs = pd.read_csv(file_path)
-        job_description = jobs.loc[jobs["job_posting_id"] == job_posting_id, "description"]
 
-        # get score 
-        raw_resume = extract_text_from_pdf(resume_path)
+        JOBS_PATH = os.path.join("backend", "data", "csvs", "jobs.csv")
+        jobs = pd.read_csv(JOBS_PATH)
+        job_description = jobs.loc[jobs["id"] == job_posting_id, "description"][0]
+
+        resume_data = db_tools.get_resumes_by_user_id(user_id)
+        resume_file_name = resume_data[0]['filename']
+        RESUME_PATH = os.path.join("backend", "data", "csvs", resume_file_name)
+        raw_resume = extract_text_from_pdf(RESUME_PATH)
+
         score_obj = Score(raw_resume, job_description)
         similarity_score = score_obj.compute_similarity()
 
