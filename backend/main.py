2ba5b39ec305eb982f6e796c452b0d0d3083e6bf
uniquedapoet <eddiebenjamin86@gmail.com>
2025-03-22 16:04:01 -0600

    Refactor debug mode in main.py and add resume download and view endpoints in resumes.py

--- backend/main.py
+++ backend/main.py
@@ -22,4 +22,4 @@ app.register_blueprint(resumes, url_prefix="/resumes")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=True)
